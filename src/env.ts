7e8b6a84a29d249c17f3e818bc85f958b89634d5
w3 <200134550+w3cdo@users.noreply.github.com>
2026-09-23 21:16:38 -0700

    build: :green_heart: don't require env to build

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,13 @@
+import { building } from "$app/env";
 import { defineEnvVars } from "@sveltejs/kit/env";
+import z from "zod";
+
+function optionalWhenBuilding<T extends z.ZodType>(i: T) {
+    return building ? z.optional(i) : i
+}
 
 export const variables = defineEnvVars({
-    BOT_TOKEN: {},
-    GUILD_ID: {public: true},
-    INVITE_CODE: {public: true}
+    BOT_TOKEN: {schema: optionalWhenBuilding(z.string())},
+    GUILD_ID: {public: true, schema: optionalWhenBuilding(z.string())},
+    INVITE_CODE: {public: true, schema: optionalWhenBuilding(z.string())}
 })
